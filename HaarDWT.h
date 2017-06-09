ce0258f8f780ee30f9f11dd84d23659f67a6745b
fauzanzaid <fauzanzaid@gmail.com>
2017-06-09 14:51:41 +0530

    Initial commit

---
+++ HaarDWT.h
@@ -0,0 +1,8 @@
+#ifndef includeGuard_3dbc44437e9e477ca3b586e6000815ad
+#define includeGuard_3dbc44437e9e477ca3b586e6000815ad
+
+void coef_1D_HaarDWT(float *ip, float *op, long n);
+void coef_1D_if_HaarDWT(int *ip, float *op, long n);
+void sig_1D_HaarDWT(float *ip, float *op, long n);
+
+#endif
