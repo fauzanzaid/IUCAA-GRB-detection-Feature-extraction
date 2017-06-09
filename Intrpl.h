1d37393b94b24788a40d17a67239b3da3af598a3
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-06-09 16:59:18 +0530

    Initial commit

---
+++ Intrpl.h
@@ -0,0 +1,7 @@
+#ifndef b671c6b0292944e5baa7183f49be2808
+#define b671c6b0292944e5baa7183f49be2808
+
+int linear_Intrpl(float *ip, int *ipsz, float *op, int *opsz);
+int cubic_Intrpl(float *ip, int *ipsz, float *op, int *opsz);
+
+#endif
