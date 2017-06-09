23e511a8e667f81b47bf84e88cc08dcad463b475
fauzanzaid <fauzanzaid@gmail.com>
2017-06-09 17:22:39 +0530

    Implement linear_Intrpl()

--- Intrpl.c
+++ Intrpl.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "Intrpl.h"
 
-int linear_Intrpl(float *ip, int *ipsz, float *op, int *opsz){
+int linear_Intrpl(float *ip, int ipsz, float *op, int opsz){
 	
-	return 0
+	float i;
+	int j;
+
+	int iI;
+	float iIF, iF;
+
+	op[0] = ip[0];	// Not in loop to avoid possible fp errors at the ends
+	for(j=1; j<opsz-1; j++){
+		i = ((float)j)*(ipsz-1)/(opsz-1);
+		iF = modff(i, &iIF);
+		iI = (int)iIF;
+		op[j] = (1-iF)*ip[iI] + iF*ip[iI+1];
+	}
+	op[opsz-1] = ip[ipsz-1];	// Not in loop to avoid possible fp errors at the ends
+
+	return 0;
 }
 
-int cubic_Intrpl(float *ip, int *ipsz, float *op, int *opsz){
+int cubic_Intrpl(float *ip, int ipsz, float *op, int opsz){
 	
-	return 0
+	return 0;
 }
