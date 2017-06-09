eca438c3ea087e18f1e9df427fc411052818c114
fauzanzaid <fauzanzaid@gmail.com>
2017-06-09 16:59:18 +0530

    Initial commit

---
+++ Intrpl.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Intrpl.h"
+
+int linear_Intrpl(float *ip, int *ipsz, float *op, int *opsz){
+	
+	return 0
+}
+
+int cubic_Intrpl(float *ip, int *ipsz, float *op, int *opsz){
+	
+	return 0
+}
