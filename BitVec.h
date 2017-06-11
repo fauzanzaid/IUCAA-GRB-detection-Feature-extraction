bc6c30878e69e1be832177c52168b32a32b2711f
fauzanzaid <fauzanzaid@gmail.com>
2017-06-11 14:23:58 +0530

    Implement functions

--- BitVec.h
+++ BitVec.h
@@ -1,16 +1,15 @@
 #ifndef c220eba242bf422fbae8bc3ae4e55552
 #define c220eba242bf422fbae8bc3ae4e55552
 
-typedef struct BitVec{
-	char *array;
-	int s;
-} BitVec;
+#include <stdint.h>
 
-BitVec new_BitVec(int sz);
-void free_BitVec(BitVec bv);
+typedef struct BitVec BitVec;
 
-void setBit_BitVec(BitVec bv, int idx, int val);
-void setAll_BitVec(BitVec bv, int val);
-int getBit_BitVec(BitVec bv, int idx);
+BitVec *new_BitVec(int sz);
+void free_BitVec(BitVec *bvptr);
+
+void setBit_BitVec(BitVec *bvptr, int idx, int val);
+void setAll_BitVec(BitVec *bvptr, int val);
+int getBit_BitVec(BitVec *bvptr, int idx);
 
 #endif
