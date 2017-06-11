0c6f19884e24e107ec23bcf621c746281e4c03f5
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-06-11 14:23:58 +0530

    Implement functions

--- BitVec.c
+++ BitVec.c
@@ -1,29 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "BitVec.h"
 
 
-BitVec new_BitVec(int sz){
-	BitVec bv;
-	return bv;
+
+typedef struct BitVec{
+	uint8_t *arr;
+	int sz;
+} BitVec;
+
+
+
+BitVec *new_BitVec(int sz){
+	BitVec *bvptr = malloc(sizeof(BitVec));
+
+	int arrsz = (sz+7)/8;
+	bvptr->arr = malloc(arrsz*sizeof(uint8_t));
+
+	memset(bvptr->arr, 0, arrsz*sizeof(uint8_t));
+	bvptr->sz = sz;
+
+	return bvptr;
 }
 
-void free_BitVec(BitVec bv){
-
+void free_BitVec(BitVec *bvptr){
+	free(bvptr->arr);
+	free(bvptr);
 }
 
 
 
-void setBit_BitVec(BitVec bv, int idx, int val){
-
+void setBit_BitVec(BitVec *bvptr, int idx, int val){
+	int arridx = idx/8;
+	int bitidx = idx%8;
+	
+	if(val==0)
+		(bvptr->arr)[arridx] &= ~(1<<bitidx);
+	else if(val==1)
+		(bvptr->arr)[arridx] |= 1<<bitidx;
 }
 
-void setAll_BitVec(BitVec bv, int val){
-
+void setAll_BitVec(BitVec *bvptr, int val){
+	int arrsz = (bvptr->sz+7)/8;
+	if(val==0)
+		memset(bvptr->arr, 0, arrsz*sizeof(uint8_t));
+	else if(val==1)
+		memset(bvptr->arr, 255, arrsz*sizeof(uint8_t));
 }
 
-int getBit_BitVec(BitVec bv, int idx){
+int getBit_BitVec(BitVec *bvptr, int idx){
 	int val;
-	return val;
+	int arridx = idx/8;
+	int bitidx = idx%8;
+	val = bvptr->arr[arridx] & 1<<bitidx;
+	
+	if(val==0)
+		return 0;
+	else
+		return 1;
 }
