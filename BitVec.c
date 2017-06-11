4e3cd232e219c1a0b9678dbd5834931d9b90200f
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-06-11 13:12:24 +0530

    Initial commit

---
+++ BitVec.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "BitVec.h"
+
+
+BitVec new_BitVec(int sz){
+	BitVec bv;
+	return bv;
+}
+
+void free_BitVec(BitVec bv){
+
+}
+
+
+
+void setBit_BitVec(BitVec bv, int idx, int val){
+
+}
+
+void setAll_BitVec(BitVec bv, int val){
+
+}
+
+int getBit_BitVec(BitVec bv, int idx){
+	int val;
+	return val;
+}
