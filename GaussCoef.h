e9ca0be88d34a72f35d51fc5244f33b1c9a0e245
fauzanzaid <fauzanzaid@gmail.com>
2017-06-12 15:39:05 +0530

    Increase coef upto R=5

--- GaussCoef.h
+++ GaussCoef.h
@@ -1,7 +1,7 @@
 #ifndef includeGuard_1578e50c67e341ccbde0e2dd90aa2159
 #define includeGuard_1578e50c67e341ccbde0e2dd90aa2159
 
-const int MAX_GAUSS_R = 3;
+const int MAX_GAUSS_R = 5;
 
 const float
 GaussCoeff_0[1]={
@@ -27,13 +27,39 @@ GaussCoeff_3[7]={
 	1.0/64*15,
 	1.0/64*6,
 	1.0/64*1
+},
+GaussCoeff_4[9]={
+	1.0/256*1,
+	1.0/256*8,
+	1.0/256*28,
+	1.0/256*56,
+	1.0/256*70,
+	1.0/256*56,
+	1.0/256*28,
+	1.0/256*8,
+	1.0/256*1
+},
+GaussCoeff_5[11]={
+	1.0/1024*1,
+	1.0/1024*10,
+	1.0/1024*45,
+	1.0/1024*120,
+	1.0/1024*210,
+	1.0/1024*252,
+	1.0/1024*210,
+	1.0/1024*120,
+	1.0/1024*45,
+	1.0/1024*10,
+	1.0/1024*1
 };
 
-const float *GaussCoeff[4] = {
+const float *GaussCoeff[6] = {
 	GaussCoeff_0,
 	GaussCoeff_1,
 	GaussCoeff_2,
-	GaussCoeff_3
+	GaussCoeff_3,
+	GaussCoeff_4,
+	GaussCoeff_5
 };
 
 #endif
