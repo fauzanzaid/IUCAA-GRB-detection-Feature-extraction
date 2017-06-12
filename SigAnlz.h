f5e7b53f15a29b3d818b9f3f8c04d9a0f39eb78b
fauzanzaid <fauzanzaid@gmail.com>
2017-06-12 15:40:35 +0530

    gaus->gauss

--- SigAnlz.h
+++ SigAnlz.h
@@ -7,6 +7,6 @@ void findBase_SigAnlz(float* sig, int n, BitVec *bvptr, float k, int niter);
 void param_SigAnlz(float *sig, int n, BitVec *bvptr, float *mean, float *rms, float *stddev);
 
 void boxBlur_SigAnlz(float* ip, float *op, int n, int rad);
-void gausBlur_SigAnlz(float* ip, float *op, int n, int rad);
+void gaussBlur_SigAnlz(float* ip, float *op, int n, int rad);
 
 #endif
