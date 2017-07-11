6518bf3e13376a226e654dbb69a02475b761d275
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-07-11 18:47:30 +0530

    Remove leading and trailing zeroes. Print status message

--- tools/fitsevt.py
+++ tools/fitsevt.py
@@ -13,11 +13,14 @@ binSize = int(sys.argv[5])
 
 fnames = os.listdir(inputFolder)
 
-for fname in fnames:
-	print(fname)
+for fidx, fname in enumerate(fnames):
+
 	hdulist = fits.open(inputFolder+"/"+fname)
-	
+
 	for i in range(1,5):
+
+		print("{0} of {1}\t{2} Q{3}".format(fidx, len(fnames), fname, i), end="\r")
+
 		timeRange = hdulist[i].header["TSTOP"] - hdulist[i].header["TSTART"]
 		nBins = math.ceil(timeRange/binSize)
 		count = [0]*nBins
@@ -27,10 +30,16 @@ for fname in fnames:
 				index = math.floor( nBins*(event["Time"] - hdulist[i].header["TSTART"])/timeRange )
 				count[index] += 1
 
+		# Remove leading and trailing 0s
+		while count and count[0] is 0:
+			count.pop(0)
+		while count and count[-1] is 0:
+			count.pop(-1)
+
 
 		sigClass = 1
 		outputFile = outputFolder+"/"+os.path.splitext(fname)[0]+"_Q{0}.txt".format(i)
 		with open(outputFile,'w') as f:
 			f.write("{0} {1}\n".format(nBins,sigClass))
-			for j in range(nBins):
-				f.write("{0}\n".format(count[j]))
+			for value in count:
+				f.write("{0}\n".format(value))
